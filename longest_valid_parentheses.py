7435c4de792c33550324133c03b3d093e8eb720e
Greeshma Rao Manikonda <greeshmamanikonda1@gmail.com>
2026-02-05 20:36:49 +0530

    Create longest_valid_parentheses.py
    Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
    
    Example :
    Input: s = "(()"
    Output: 2
    Explanation: The longest valid parentheses substring is "()".

---
+++ longest_valid_parentheses.py
@@ -0,0 +1,13 @@
+stack = [-1]
+        max_len = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                stack.append(i)
+            else:  # c == ')'
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
+        
+        return max_len
